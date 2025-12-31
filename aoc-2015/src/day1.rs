69e9dc4305e78cb8fe4b4fb2c1e65d0980cac0c8
BlitzDestroyer <realblitzdestroyer@gmail.com>
2025-12-30 20:45:59 -0600

    solve day1 2015

---
+++ aoc-2015/src/day1.rs
@@ -0,0 +1,56 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum PuzzleError {
+    #[error("IO Error: {0}")]
+    Io(#[from] std::io::Error),
+}
+
+pub fn solve_day1_puzzle_part1() -> Result<(), PuzzleError> {
+    let input = std::fs::read_to_string("inputs/day1.txt")?;
+    let mut floor = 0;
+    let chars = input.chars();
+    for c in chars {
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => (),
+        }
+    }
+
+    println!("Final floor: {}", floor);
+    
+    Ok(())
+}
+
+pub fn solve_day1_puzzle_part2() -> Result<(), PuzzleError> {
+    let input = std::fs::read_to_string("inputs/day1.txt")?;
+    let mut floor = 0;
+    let chars = input.chars();
+    for (i, c) in chars.enumerate() {
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => (),
+        }
+
+        if floor == -1 {
+            println!("Position entering basement: {}", i + 1);
+            break;
+        }
+    }
+
+    println!("Final floor: {}", floor);
+    
+    Ok(())
+}
+
+#[test]
+fn test_day1_puzzle_part1() {
+    assert!(solve_day1_puzzle_part1().is_ok());
+}
+
+#[test]
+fn test_day1_puzzle_part2() {
+    assert!(solve_day1_puzzle_part2().is_ok());
+}
