72fc1ec6259b8d6509fee74c3795b1a5ea7c35db
BlitzDestroyer <realblitzdestroyer@gmail.com>
2025-12-30 21:11:17 -0600

    solve day4 2015

---
+++ aoc-2015/src/day4.rs
@@ -0,0 +1,49 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum PuzzleError {
+    #[error("IO Error: {0}")]
+    Io(#[from] std::io::Error),
+}
+
+pub fn solve_day4_puzzle_part1() -> Result<(), PuzzleError> {
+    let input = std::fs::read_to_string("inputs/day4.txt")?;
+    let secret_key = input.trim();
+
+    for i in 0.. {
+        let hash_input = format!("{}{}", secret_key, i);
+        let digest = md5::compute(hash_input);
+        if digest[0] == 0 && digest[1] == 0 && (digest[2] & 0xF0) == 0 {
+            println!("Lowest number for five leading zeroes: {}", i);
+            break;
+        }
+    }
+
+    Ok(())
+}
+
+pub fn solve_day4_puzzle_part2() -> Result<(), PuzzleError> {
+    let input = std::fs::read_to_string("inputs/day4.txt")?;
+    let secret_key = input.trim();
+
+    for i in 0.. {
+        let hash_input = format!("{}{}", secret_key, i);
+        let digest = md5::compute(hash_input);
+        if digest[0] == 0 && digest[1] == 0 && digest[2] == 0 {
+            println!("Lowest number for six leading zeroes: {}", i);
+            break;
+        }
+    }
+
+    Ok(())
+}
+
+#[test]
+pub fn test_solve_day4_puzzle_part1() -> Result<(), PuzzleError> {
+    solve_day4_puzzle_part1()
+}
+
+#[test]
+pub fn test_solve_day4_puzzle_part2() -> Result<(), PuzzleError> {
+    solve_day4_puzzle_part2()
+}
