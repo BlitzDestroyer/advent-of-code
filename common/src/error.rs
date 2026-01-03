a388367f8dc805413a8ad1e16df99d0b3ed29c0b
BlitzDestroyer <realblitzdestroyer@gmail.com>
2026-01-03 15:38:48 -0600

    add common package

---
+++ common/src/error.rs
@@ -0,0 +1,9 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum PuzzleError {
+    #[error("IO Error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("JSON Error: {0}")]
+    Json(#[from] serde_json::Error),
+}
